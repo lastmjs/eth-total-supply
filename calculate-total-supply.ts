7620ed8216f04daca6d650e80ab5351578113f20
Jordan Last <jordan.michael.last@gmail.com>
2020-08-11 22:30:26 -0600

    make more declarative, start adding file and directory structure. Get blocks with uncles

--- calculate-total-supply.ts
+++ calculate-total-supply.ts
@@ -17,29 +17,57 @@
 // TODO more work checking by Nethermind: https://github.com/NethermindEth/nethermind/pull/2193/files
 // TODO https://docs.nethermind.io/nethermind/guides-and-helpers/custom-analytic-tools
 
+// TODO also check my work here: https://github.com/CurrencyTycoon/mysupplyaudit
+
+// TODO verifying block reward information, we want to be able to point to this information in all of the major clients for verification
+// TODO This might be where geth gets the transition to the new values: https://github.com/ethereum/go-ethereum/blob/master/params/config.go
+// This is where geth gets the reward values: https://github.com/ethereum/go-ethereum/blob/master/consensus/ethash/consensus.go
+
 import * as fetch from 'node-fetch';
 import * as fs from 'fs';
 import { BigNumber } from 'bignumber.js';
 
 type WEI = BigNumber;
 type ETH = BigNumber;
-
-type GenesisBlockDistribution = {
-    [ethAddress: string]: {
-        wei: string;
-    };
-};
+type HexString = string;
 
 type NethermindGenesisBlockDistribution = {
     [ethAddress: string]: {
-        balance?: string;
+        readonly balance?: string;
     };
 };
 
 type OpenEthereumGenesisBlockDistribution = {
     [ethAddress: string]: {
-        balance?: string;
+        readonly balance?: string;
     };
+};
+
+type BlockResult = {
+    readonly result: Readonly<Block>;
+};
+
+type UncleBlockResult = {
+    readonly result: Readonly<UncleBlock>;
+};
+
+type Block = {
+    readonly number: HexString;
+    readonly uncles: Array<HexString>;
+};
+
+type BlockWithUncleBlocks = {
+    readonly number: HexString;
+    readonly uncles: ReadonlyArray<UncleBlock>;
+};
+
+type UncleBlock = {
+    readonly number: HexString;
+    readonly hash: HexString;
+};
+
+type UncleBlockMap = {
+    [uncleHash: string]: HexString;
 };
 
 (async () => {
@@ -52,9 +80,14 @@ type OpenEthereumGenesisBlockDistribution = {
     console.log('genesisSupplyInETH', genesisSupplyInETH.toString());
 
     // // const latestBlockNumber: number = await getLatestBlockNumber();
-    const latestBlockNumber: number = 1778436;
+    const latestBlockNumber: number = 10;
 
     console.log('latestBlockNumber', latestBlockNumber);
+
+    const blocksWithUncleBlocks: ReadonlyArray<BlockWithUncleBlocks> = await getBlocksWithUncleBlocks(1, latestBlockNumber);
+
+    console.log('blocksWithUncleBlocks', JSON.stringify(blocksWithUncleBlocks, null, 2));
+    console.log('blocksWithUncleBlocks.length', blocksWithUncleBlocks.length);
 
     // const blockRewardSupply: WEI = calculateBlockRewardSupply(latestBlockNumber);
 
@@ -81,10 +114,8 @@ type OpenEthereumGenesisBlockDistribution = {
     // console.log('totalSupplyInETH', totalSupplyInETH.toString());
 })();
 
-// TODO add in the config information from parity-ethereum
 function calculateGenesisSupply(): WEI {
     // const genesisSupplyFromGethConfig: WEI = calculateGenesisSupplyFromGethConfig();
-    // const genesisSupplyFromParityConfig: WEI = calculateGenesisSupplyFromParityConfig();
     const genesisSupplyFromOpenEthereumConfig: WEI = calculateGenesisSupplyFromOpenEthereumConfig();
     const genesisSupplyFromNethermindConfig: WEI = calculateGenesisSupplyFromNethermindConfig();
 
@@ -100,11 +131,6 @@ function calculateGenesisSupply(): WEI {
 // function calculateGenesisSupplyFromGethConfig(): WEI {
 //     return new BigNumber(0);
 // }
-
-// TODO Parity might be Open Ethereum now
-function calculateGenesisSupplyFromParityConfig(): WEI {
-    return new BigNumber(0);
-}
 
 function calculateGenesisSupplyFromOpenEthereumConfig(): WEI {
     // The open-ethereum-foundation.json file was taken from the Open Ethereum GitHub repository
@@ -141,6 +167,7 @@ function calculateGenesisSupplyFromNethermindConfig(): WEI {
 }
 
 // TODO we might need to get this information from the blockchain directly for ultimate assurance
+// TODO we need to document very clearly where the amounts and block numbers come from in multiple clients
 function getBlockReward(blockNumber: number): WEI {
     if (
         blockNumber >= 0 &&
@@ -178,42 +205,130 @@ function getUncleReward(blockNumber: number): WEI {
     return new BigNumber(1.75).times(10**18);
 }
 
-// TODO figure out how to pageinate the uncle thing here
-async function getNumUnclesForBlocks(
+async function getBlocksWithUncleBlocks(
     startBlockNumber: number,
     endBlockNumber: number,
-    currentBlockNumber: number = 0,
-    skip: number = 10000
-): Promise<Array<number>> {
+    currentStartBlockNumber: number = startBlockNumber,
+    skip: number = 10000,
+    allBlocks: ReadonlyArray<BlockWithUncleBlocks> = []
+): Promise<ReadonlyArray<BlockWithUncleBlocks>> {
 
-    // const startIndex =
+    const currentEndBlockNumber: number = currentStartBlockNumber + skip - 1 < endBlockNumber ? currentStartBlockNumber + skip - 1 : endBlockNumber;
 
-    const body = new Array(endBlockNumber - startBlockNumber).fill(0).map((_, index) => {
+    console.log('currentStartBlockNumber', currentStartBlockNumber);
+    console.log('currentEndBlockNumber', currentEndBlockNumber);
+
+    const someBlocksWithUncleBlocks: ReadonlyArray<BlockWithUncleBlocks> = await fetchBlocksWithUncleBlocks(currentStartBlockNumber, currentEndBlockNumber);
+
+    const augmentedBlocksWithUncleBlocks = [...allBlocks, ...someBlocksWithUncleBlocks];
+
+    if (currentEndBlockNumber === endBlockNumber) {
+        return augmentedBlocksWithUncleBlocks;
+    }
+    else {
+        return await getBlocksWithUncleBlocks(startBlockNumber, endBlockNumber, currentStartBlockNumber + skip, skip, augmentedBlocksWithUncleBlocks);
+    }
+}
+
+async function fetchBlocksWithUncleBlocks(startBlockNumber: number, endBlockNumber: number): Promise<ReadonlyArray<BlockWithUncleBlocks>> {
+    const blocks: ReadonlyArray<Block> = await fetchBlocks(startBlockNumber, endBlockNumber);
+    const uncleBlocks: ReadonlyArray<UncleBlock> = await fetchUncleBlocks(blocks);
+    const blocksWithUncleBlocks: ReadonlyArray<BlockWithUncleBlocks> = combineBlocksWithUncleBlocks(blocks, uncleBlocks);
+
+    return blocksWithUncleBlocks;
+}
+
+async function fetchBlocks(startBlockNumber: number, endBlockNumber: number): Promise<ReadonlyArray<Block>> {
+    const requestBody = new Array(endBlockNumber - startBlockNumber + 1).fill(0).map((_, index) => {
         return {
             id: 1,
             jsonrpc: '2.0',
-            method: 'eth_getUncleCountByBlockNumber',
+            method: 'eth_getBlockByNumber',
             params: [
-                `0x${new Number(startBlockNumber + index).toString(16)}`
+                `0x${new Number(startBlockNumber + index).toString(16)}`,
+                false
             ]
         };
     });
 
-    const result = await fetch('http://localhost:8545', {
+    const response = await fetch('http://localhost:8545', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(body)
+        body: JSON.stringify(requestBody)
     });
 
-    const resultJSON = await result.json();
+    const responseJSON = await response.json();
 
-    console.log(resultJSON);
-
-    return resultJSON.map((result: any) => {
-        return parseInt(resultJSON.result);
+    const retrievedBlocks: ReadonlyArray<Block> = responseJSON.map((blockResult: Readonly<BlockResult>) => {
+        return blockResult.result;
     });
+
+    return retrievedBlocks;
+}
+
+async function fetchUncleBlocks(blocks: ReadonlyArray<Block>): Promise<ReadonlyArray<UncleBlock>> {
+
+    const requestBody = blocks.map((block: Readonly<Block>) => {
+        return [...block.uncles.map((uncleHash: string, index: number) => {
+            return {
+                id: 1,
+                jsonrpc: '2.0',
+                method: 'eth_getUncleByBlockNumberAndIndex',
+                params: [
+                    block.number,
+                    `0x${new Number(index).toString(16)}`
+                ]
+            };
+        })];
+    }).flat(); // TODO add the TypeScript config to get rid of this error
+
+    const response = await fetch('http://localhost:8545', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(requestBody)
+    });
+
+    const responseJSON = await response.json();
+
+    const retrievedUncleBlocks: ReadonlyArray<UncleBlock> = responseJSON.map((uncleBlockResult: Readonly<UncleBlockResult>) => {
+        return uncleBlockResult.result;
+    });
+
+    return retrievedUncleBlocks;
+}
+
+function combineBlocksWithUncleBlocks(
+    blocks: ReadonlyArray<Block>,
+    uncleBlocks: ReadonlyArray<UncleBlock>
+): ReadonlyArray<BlockWithUncleBlocks> {
+    const uncleBlocksMap: Readonly<UncleBlockMap> = createUncleBlockMap(uncleBlocks);
+    const blocksWithUncleBlocks: ReadonlyArray<BlockWithUncleBlocks> = blocks.map((block: Readonly<Block>) => {
+        const blockWithUncleBlocks: Readonly<BlockWithUncleBlocks> = {
+            ...block,
+            uncles: block.uncles.map((uncleHash: HexString) => {
+                return {
+                    hash: uncleHash,
+                    number: uncleBlocksMap[uncleHash]
+                };
+            })
+        };
+        return blockWithUncleBlocks;
+    });
+
+    return blocksWithUncleBlocks;
+}
+
+function createUncleBlockMap(uncleBlocks: ReadonlyArray<UncleBlock>): Readonly<UncleBlockMap> {
+    return uncleBlocks.reduce((uncleBlockMap: Readonly<UncleBlockMap>, uncleBlock: Readonly<UncleBlock>) => {
+        return {
+            ...uncleBlockMap,
+            [uncleBlock.hash]: uncleBlock.number
+        };
+    }, {});
 }
 
 async function getLatestBlockNumber(): Promise<number> {
@@ -234,18 +349,18 @@ async function getLatestBlockNumber(): Promise<number> {
     return parseInt(resultJSON.result);
 }
 
-function calculateBlockRewardSupply(latestBlockNumber: number): WEI {
-    const blockRewardSupplyFrom0To4369999: WEI = new BigNumber(4370000).times(getBlockReward(0));
-    const blockRewardSupplyFrom437000To7280000: WEI = new BigNumber(2910000).times(getBlockReward(4370000));
-    const blockRewardSupplyRemaining: WEI = new BigNumber(latestBlockNumber - 7280000).times(getBlockReward(7280000));
+// function calculateBlockRewardSupply(latestBlockNumber: number): WEI {
+//     const blockRewardSupplyFrom0To4369999: WEI = new BigNumber(4370000).times(getBlockReward(0));
+//     const blockRewardSupplyFrom437000To7280000: WEI = new BigNumber(2910000).times(getBlockReward(4370000));
+//     const blockRewardSupplyRemaining: WEI = new BigNumber(latestBlockNumber - 7280000).times(getBlockReward(7280000));
 
-    return blockRewardSupplyFrom0To4369999.plus(blockRewardSupplyFrom437000To7280000).plus(blockRewardSupplyRemaining);
-}
+//     return blockRewardSupplyFrom0To4369999.plus(blockRewardSupplyFrom437000To7280000).plus(blockRewardSupplyRemaining);
+// }
 
 // TODO I think the uncle reward calculations might be more complicated than I thought
-async function calculateUncleRewardSupply(latestBlockNumber: number): Promise<WEI> {
-    const numUnclesForBlocks: Array<number> = await getNumUnclesForBlocks(1, latestBlockNumber);
-    return numUnclesForBlocks.reduce((sum: WEI, numUncles: number, index: number) => {
-        return sum.plus(getUncleReward(index).times(numUncles));
-    }, new BigNumber(0));
-}
+// async function calculateUncleRewardSupply(latestBlockNumber: number): Promise<WEI> {
+//     const numUnclesForBlocks: Array<number> = await getNumUnclesForBlocks(1, latestBlockNumber);
+//     return numUnclesForBlocks.reduce((sum: WEI, numUncles: number, index: number) => {
+//         return sum.plus(getUncleReward(index).times(numUncles));
+//     }, new BigNumber(0));
+// }
